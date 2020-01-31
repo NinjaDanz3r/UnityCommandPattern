8ff2d67dfd499f9f90423f8869c3e9c6e3669f05
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2020-01-31 17:00:08 +0100

    Published package to branch.

---
+++ UnityPatterns/Tests/Runtime/CommandPatternSimpleTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using GenericUnityPatterns.CommandPattern;
+using System;
+
+namespace Tests
+{
+    public class CommandPatternSimpleTests
+    {
+        private class TestClass
+        {
+            public int number = 0;
+
+            public void IncreaseNumber()
+            {
+                number++;
+            }
+
+            public void DecreaseNumber()
+            {
+                number--;
+            }
+
+            public void SetNumber(int number)
+            {
+                this.number = number;
+            }
+        }
+
+        private static class TestHelper
+        {
+            public static List<ICommand> GenerateMixedCommands<T>(int n, T target, Action<T> execute, Action<T> undo)
+            {
+                var commands = new List<ICommand>();
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i % 2 == 0)
+                        commands.Add(new SerializableCommand<T>(target, execute));
+                    else
+                        commands.Add(new SerializableReversibleCommand<T>(target, execute, undo));
+                }
+                return commands;
+            }
+        }
+
+        [Test]
+        public void TestExecute()
+        {
+            var testClass = new TestClass();
+            var command = new SerializableCommand<TestClass>(testClass, t => t.IncreaseNumber());
+            command.Execute();
+            Assert.AreEqual(1, testClass.number);
+        }
+
+        [Test]
+        public void TestExecuteAndUndo()
+        {
+            var testClass = new TestClass();
+            var command = new SerializableReversibleCommand<TestClass>(testClass, t => t.IncreaseNumber(), t => t.DecreaseNumber());
+            command.Execute();
+            command.UndoExecute();
+            Assert.AreEqual(0, testClass.number);
+        }
+
+        [Test]
+        public void TestExecuteAndUndoAdvanced()
+        {
+            var testClass = new TestClass();
+            testClass.number = 10;
+
+            // Please note that we copy the old number, otherwise if we would set the undo action as following:
+            // t => t.SetNumber(t.number) will actually call SetNumber with a reference to the t class!
+            int oldNumber = testClass.number;
+            var command = new SerializableReversibleCommand<TestClass>(testClass, t => t.SetNumber(77), t => t.SetNumber(oldNumber));
+            Assert.AreEqual(10, testClass.number);
+
+            command.Execute();
+            Assert.AreEqual(77, testClass.number);
+
+            command.UndoExecute();
+            Assert.AreEqual(10, testClass.number);
+        }
+
+        [Test]
+        public void TestMixMatchExecute()
+        {
+            var testClass = new TestClass();
+            var commands = TestHelper.GenerateMixedCommands(10, testClass, t => t.IncreaseNumber(), t => t.DecreaseNumber());
+
+            foreach (var command in commands)
+            {
+                command.Execute();
+            }
+            Assert.AreEqual(10, testClass.number);
+        }
+
+        [Test]
+        public void TestMixMatchExecuteUndo()
+        {
+            var testClass = new TestClass();
+            var commands = TestHelper.GenerateMixedCommands(10, testClass, t => t.IncreaseNumber(), t => t.DecreaseNumber());
+
+            foreach (var command in commands)
+            {
+                command.Execute();
+                if (command is IReversibleCommand)
+                    (command as IReversibleCommand).UndoExecute();
+            }
+
+            Assert.AreEqual(5, testClass.number);
+        }
+    }
+}
---
+++ UnityPatterns/Tests/Runtime/CommandSingletonStrategyTests.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using GenericUnityPatterns.CommandPattern;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Tests
+{
+    public class SingletonCommandStrategyTests
+    {
+        #region Private classes
+        /// <summary>
+        /// Test class representing a game's state, as a singleton.
+        /// </summary>
+        [Serializable]
+        private class TestGameState
+        {
+            public int numberOfEnemies = 0;
+            public int numberOfPlayers = 0;
+            private static TestGameState instance = null;
+
+            private TestGameState() { }
+
+            public static void ResetState()
+            {
+                Instance.numberOfEnemies = 0;
+                Instance.numberOfPlayers = 0;
+            }
+
+            public static TestGameState Instance
+            {
+                get
+                {
+                    if (instance == null)
+                    {
+                        instance = new TestGameState();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Class that operates on the game state.
+        /// </summary>
+        [Serializable]
+        private class GameStateChanger
+        {
+            public void IncreaseNumberOfEnemies()
+            {
+                TestGameState.Instance.numberOfEnemies++;
+            }
+
+            public void SetNumberOfPlayers(int number)
+            {
+                TestGameState.Instance.numberOfPlayers = number;
+            }
+        }
+
+        /// <summary>
+        /// Class that through a method call operates on the game state.
+        /// </summary>
+        [Serializable]
+        private class MethodGameStateChanger
+        {
+            TestGameState state;
+            int numberOfPlayers;
+            int numberOfEnemies;
+
+            public void SetPlayersAndEnemies(TestGameState state, int numberOfPlayers, int numberOfEnemies)
+            {
+                state.numberOfPlayers = numberOfPlayers;
+                state.numberOfEnemies = numberOfEnemies;
+            }
+        }
+
+        /// <summary>
+        /// A simple helper class that helps us avoid DRY code.
+        /// </summary>
+        private static class TestHelper
+        {
+            public static ICommand GeneratePlayerAndEnemiesCommand()
+            {
+                var methodGameStateChanger = new MethodGameStateChanger();
+                return new SerializableCommand<MethodGameStateChanger>
+                (methodGameStateChanger, changer => changer.SetPlayersAndEnemies(TestGameState.Instance, 1, 2));
+            }
+        }
+
+        #endregion
+
+        [SetUp]
+        public void BaseSetup()
+        {
+            TestGameState.ResetState();
+        }
+
+        [Test]
+        public void TestOperateOnState()
+        {
+            var gameStateChanger = new GameStateChanger();
+            var increaseNumberOfEnemiesCommand = new SerializableCommand<GameStateChanger>
+                ( gameStateChanger, changer => changer.IncreaseNumberOfEnemies());
+
+            increaseNumberOfEnemiesCommand.Execute();
+
+            Assert.AreEqual(1, TestGameState.Instance.numberOfEnemies);
+            Assert.AreEqual(0, TestGameState.Instance.numberOfPlayers);
+        }
+
+        [Test]
+        public void TestOperateOnStateThroughMethod()
+        {
+            var methodGameStateChanger = new MethodGameStateChanger();
+
+            var setPlayersAndEnemies = TestHelper.GeneratePlayerAndEnemiesCommand();
+
+            setPlayersAndEnemies.Execute();
+
+            Assert.AreEqual(1, TestGameState.Instance.numberOfPlayers);
+            Assert.AreEqual(2, TestGameState.Instance.numberOfEnemies);
+        }
+
+        [Test]
+        public void TestList()
+        {
+            var listOfCommands = new List<ICommand>();
+
+            var methodGameStateChanger = new MethodGameStateChanger();
+            var setPlayersAndEnemies = TestHelper.GeneratePlayerAndEnemiesCommand();
+            listOfCommands.Add(setPlayersAndEnemies);
+
+            var gameStateChanger = new GameStateChanger();
+            var increaseNumberOfEnemies = new SerializableCommand<GameStateChanger>
+                (gameStateChanger, changer => changer.IncreaseNumberOfEnemies());
+            listOfCommands.Add(increaseNumberOfEnemies);
+
+            foreach(var command in listOfCommands)
+            {
+                command.Execute();
+            }
+
+            Assert.AreEqual(1, TestGameState.Instance.numberOfPlayers);
+            Assert.AreEqual(3, TestGameState.Instance.numberOfEnemies);
+        }
+
+        [Test]
+        public void TestSerialization()
+        {
+            var listOfCommands = new List<ICommand>();
+
+            var methodGameStateChanger = new MethodGameStateChanger();
+            var setPlayersAndEnemies = TestHelper.GeneratePlayerAndEnemiesCommand();
+            listOfCommands.Add(setPlayersAndEnemies);
+
+            var gameStateChanger = new GameStateChanger();
+            var increaseNumberOfEnemies = new SerializableCommand<GameStateChanger>
+                (gameStateChanger, changer => changer.IncreaseNumberOfEnemies());
+            listOfCommands.Add(increaseNumberOfEnemies);
+
+
+            byte[] bytes = new byte[0];
+
+            using (var memoryStream = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(memoryStream, listOfCommands);
+                bytes = memoryStream.ToArray();
+            }
+
+            var newListOfCommands = new List<ICommand>();
+
+            using (var memoryStream = new MemoryStream(bytes, 0, bytes.Length))
+            {
+                memoryStream.Write(bytes, 0, bytes.Length);
+                memoryStream.Position = 0;
+                var data = new BinaryFormatter().Deserialize(memoryStream);
+                newListOfCommands = data as List<ICommand>;
+            }
+
+            foreach (var command in newListOfCommands)
+            {
+                command.Execute();
+            }
+            
+            Assert.AreEqual(1, TestGameState.Instance.numberOfPlayers);
+            Assert.AreEqual(3, TestGameState.Instance.numberOfEnemies);
+        }
+    }
+}
